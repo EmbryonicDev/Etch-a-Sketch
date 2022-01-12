32ec8b74b7034c17cd889d100a528a9afa015de4
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-12 14:52:46 +0100

    +f gridsize to create grid

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let size = 9;
+
 const mainContr = document.querySelector('#mainContr');
 const settings = document.querySelector('#settings');
 const colorBtn = document.querySelector('#colorBtn');
@@ -8,6 +10,17 @@ const clearBtn = document.querySelector('#clearBtn');
 const slideBtn = document.querySelector('#slideBtn');
 const grid = document.querySelector('#grid');
 
+function gridSize(size) {
+  grid.style.setProperty('--grid-rows', size);
+  grid.style.setProperty('--grid-cols', size);
+  for (c = 0; c < (size * size); c++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (c + 1);
+    grid.appendChild(cell).className = "gridItem";
+  };
+};
+
+gridSize(size);
 
 
 
