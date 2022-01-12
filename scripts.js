da5471e856c46f57028bf60ff74c8920c1490e9c
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-12 12:02:05 +0100

    query elements

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const mainContr = document.querySelector('#mainContr');
+const settings = document.querySelector('#settings');
+const colorBtn = document.querySelector('#colorBtn');
+const flashBtn = document.querySelector('#flashBtn');
+const rainbowBtn = document.querySelector('#rainbowBtn');
+const eraserBtn = document.querySelector('#eraserBtn');
+const clearBtn = document.querySelector('#clearBtn');
+const slideBtn = document.querySelector('#slideBtn');
+const grid = document.querySelector('#grid');
+
+
+
+
+
+
+
