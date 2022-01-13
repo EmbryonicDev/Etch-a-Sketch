879d313f16b83d1891cf135589bebdfa156a391c
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-01-13 11:52:42 +0100

    \+eventListener to f gridBuild to change cell color with mouseover

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let size = 16;
+let brushColor = "#808080"
 
 const mainContr = document.querySelector('#mainContr');
 const settings = document.querySelector('#settings');
@@ -10,21 +11,23 @@ const clearBtn = document.querySelector('#clearBtn');
 const sizeValue = document.querySelector('#sizeValue');
 const sizeSlider = document.querySelector('#sizeSlider');
 const grid = document.querySelector('#grid');
+const cell = document.getElementsByClassName('cell');
 
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
-sizeSlider.onchange = (e) => changeSize(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value);
 
 function gridBuild(size) {
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-
   while (grid.firstChild) {
     grid.removeChild(grid.lastChild);
   }
-
   for(i = 0; i < size * size; i++) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
+    cell.addEventListener('mouseover', function(e)  {
+      e.target.style.background = brushColor;
+    });
     grid.append(cell);
   }
 }
@@ -39,8 +42,3 @@ function changeSize(newSize) {
   size = newSize;
   gridBuild(size);
 }
-
-
-
-
-
